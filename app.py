df8f3f5b2abc8bd95c16a3cc695a1b193b910c83
offenzo <mendoncaenzo2501@gmail.com>
2026-01-28 18:09:51 -0300

    correcao do codigo

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ def contadorDeCaracter ():
     print(f'Sua palavra tem {len(palavra)} caracteres.')
     decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip()
     if decisao == 'S':
-        print('entro')
         while decisao == 'S':
             palavra = str(input('Escreva uma palavra: '))
             print(f'Sua palavra tem {len(palavra)} caracteres.')
             decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip()
     elif decisao != 'S' and decisao != 'N':
+        print('elif')
         decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip()
         if decisao == 'S':
             while decisao == 'S':
                 palavra = str(input('Escreva uma palavra: '))
                 print(f'Sua palavra tem {len(palavra)} caracteres.')
-                decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip()
+                decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip() 
 
     return 'Fim do programa'
 resutaldo = contadorDeCaracter()
