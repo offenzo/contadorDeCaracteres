15c9c972ba20ed3a7eddec9e4ec81039d1e9b2bf
offenzo <mendoncaenzo2501@gmail.com>
2026-01-31 21:46:04 -0300

    otimizacao do codigo

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 def contadorDeCaracter ():
-    palavra = str(input('Escreva uma palavra: '))
-    print(f'Sua palavra tem {len(palavra)} caracteres.')
-    decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip()
-    if decisao == 'S':
-        while decisao == 'S':
-            palavra = str(input('Escreva uma palavra: '))
-            print(f'Sua palavra tem {len(palavra)} caracteres.')
-            decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip()
-    elif decisao != 'S' and decisao != 'N':
+    while True:
+        palavra = str(input('Escreva uma palavra: '))
+        print(f'Sua palavra tem {len(palavra)} caracteres.')
         decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip()
         if decisao == 'S':
-            while decisao == 'S':
-                palavra = str(input('Escreva uma palavra: '))
-                print(f'Sua palavra tem {len(palavra)} caracteres.')
-                decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip() 
-
+            continue
+        elif decisao != 'S' and decisao != 'N':
+            decisao = str(input('Voce deseja continuar [S/N] ? ')) .upper() .strip()
+            if decisao == 'S':
+                continue
+            else:
+                break
+        else:
+            break
     return 'Fim do programa'
-resultado = contadorDeCaracter()
-print(resultado)
+resutaldo = contadorDeCaracter()
+print(resutaldo)
